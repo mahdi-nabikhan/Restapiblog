72d34581cd057beec2a5bad9db4a822a6e64900d
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2025-05-23 01:28:32 +0330

    Change in Accounts App  +add user admin model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+from django.contrib.auth.forms import UserChangeForm,UserCreationForm
 
-# Register your models here.
+class CustomUserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email', 'first_name', 'is_superuser', 'is_active')
+    list_filter = ('is_superuser', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'created_date', 'updated_date')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser'),
+        }),
+    )
+
+admin.site.register(User, CustomUserAdmin)

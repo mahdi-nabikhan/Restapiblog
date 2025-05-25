60959cd398e73d7f5f0b00d7d6db1f970c82215e
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2025-05-25 14:58:56 +0330

    Update Core

--- RestApiBlog/urls.py
+++ RestApiBlog/urls.py
@@ -38,14 +38,13 @@ schema_view = get_schema_view(
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls')),
-    path('accounts/',include('accounts.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # 📄 Swagger UI
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # 📘 ReDoc
+    path('blog/', include('blog.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 # serving static and media for development
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

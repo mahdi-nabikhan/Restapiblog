eadf72c251c3225420c1ef7ee727229941a84d20
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2025-06-09 14:18:10 +0330

    change in accounts app +add tests directory for testing model and serializer and view and  url + add test for model seriailzer and views

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -3,8 +3,10 @@ from .views import *
 from rest_framework.authtoken import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
+app_name = 'api-v1'
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('token/register/',SendTokenActivationRegisterView.as_view(), name='token-register'),
     path('token/login/', views.ObtainAuthToken.as_view(), name='token'),
     path('custom/token/login', CustomObtainToken.as_view(), name='custom-token'),
     path('discard/token/', CustomDiscardAuthToken.as_view(), name='discard-token'),
@@ -15,8 +17,7 @@ urlpatterns = [
     path('change/password', ChangePasswordView.as_view(), name='change-password'),
     path('profile/', ProfileApiView.as_view(), name='profile'),
     path('send/email/', SendEmailView.as_view(), name='send-email'),
-    path('send/email/template/',SendEmailApiView.as_view(),name='send_templated_email'),
-    path('activation/confirm/<str:token>/',ActivationApiView.as_view(),name='activation-confirm'),
-
+    path('send/email/template/', SendEmailApiView.as_view(), name='send_templated_email'),
+    path('activation/confirm/<str:token>/', ActivationApiView.as_view(), name='activation-confirm'),
 
 ]
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-
+app_name = 'accounts'
 urlpatterns = [
     path('api/v1/', include('accounts.api.v1.urls')),
 ]

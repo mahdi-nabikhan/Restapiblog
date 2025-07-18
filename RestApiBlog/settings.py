d186ec0aa92ded462a4085c801925133221860f8
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2025-07-18 16:13:28 +0330

    change in projects +change cache and cache config in to settings.py

--- RestApiBlog/settings.py
+++ RestApiBlog/settings.py
@@ -157,12 +157,12 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 # caching config
-CACHES ={
-    'default':{
-        'BACKEND':'django_redis.cache.RedisCache',
-        'LOCATION':'redis://127.0.0.1:6379/2',
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/2',
         'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis/client.DefaultClient'
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     }
 }

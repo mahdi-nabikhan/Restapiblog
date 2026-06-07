0020c424816bb623717e1b85c349d04db3285538
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-06-07 11:35:01 +0330

    feat(frontend):add new component named Mainbar
    -add Mainbar for better look for landing page

--- frontend/src/Pages/Main/Index/Index.jsx
+++ frontend/src/Pages/Main/Index/Index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import PostList from '../../../Components/Main/PostList/PostList'
+import MainBar from '../../../Components/Main/Mainbar/Mainbar'
 export default function Index() {
   return (
     <>
+    <MainBar/>
     <PostList/>
     </>
   )

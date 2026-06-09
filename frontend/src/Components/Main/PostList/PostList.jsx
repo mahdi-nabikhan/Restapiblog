4cfbd920e119ad3a5ba0d5e82035a1ddfa497939
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-06-09 11:44:52 +0330

    feat(frontend):load data from api for post list component feat(frontend): change style for post detail for better look(fix image layout bug )

--- frontend/src/Components/Main/PostList/PostList.jsx
+++ frontend/src/Components/Main/PostList/PostList.jsx
@@ -1,33 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./PostList.css";
 
 export default function PostList() {
+  const [posts,setPosts]=useState([])
+
+  const fetchPosts = async () => {
+    try {
+      const res = await fetch(
+        "http://localhost:8000/blog/api/v1/post/",
+        {
+          method: "GET",
+        }
+      );
+
+      const data = await res.json();
+      setPosts(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+
   return (
     <div className="posts-container">
-      <h2 className="posts-title">Latest Posts</h2>
+  <h2 className="posts-title">Latest Posts</h2>
 
-      <div className="posts-grid">
-        <div className="post-card">
-          <h3>Post Title 1</h3>
-          <p>
-            This is a short description for the first post. It shows preview text.
-          </p>
-        </div>
+  <div className="posts-grid">
+    {posts.map((post) => (
+      <div className="post-card" key={post.id}>
 
-        <div className="post-card">
-          <h3>Post Title 2</h3>
-          <p>
-            This is a short description for the second post. It shows preview text.
-          </p>
-        </div>
+        <img
+          className="post-image"
+          src={post.image || "/images/default_image.PNG"}
+          alt={post.title}
+          onError={(e) => {
+            e.target.src = "/images/default_image.PNG";
+          }}
+        />
 
-        <div className="post-card">
-          <h3>Post Title 3</h3>
-          <p>
-            This is a short description for the third post. It shows preview text.
-          </p>
-        </div>
+        <h3>Title : {post.title}</h3>
+
+        <p>
+          Description : {post.snippet}
+        </p>
+        <p>Published at : {post.created_date}</p>
+        
+        <Link to={`post/${post.id}`}>Detail</Link>
+        
       </div>
-    </div>
+    ))}
+  </div>
+</div>
   );
 }

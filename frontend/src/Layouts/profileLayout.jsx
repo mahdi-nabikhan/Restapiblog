8c51da0a02a3a5f6166cc93152664fd48704638c
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-06-10 11:35:28 +0330

    feat(frontend):add new component for showing  post of ralated user feat(frontend):add  new stylea and look to Profile layouts

--- frontend/src/Layouts/profileLayout.jsx
+++ frontend/src/Layouts/profileLayout.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import TopBar from '../Components/Panel/TopBar/TopBar'
 import Sidebar from '../Components/Panel/SideBar/SideBar'
-
+import './ProfileLayout.css'
 export default function ProfileLayout() {
   return (
     <>
-    <TopBar/>
-    <Sidebar/>
-    <main>
-        <Outlet/>
-    </main>
-    
+     <div className="panel-layout">
+      <Sidebar />
+
+      <div className="panel-main">
+        <TopBar />
+        <main className="panel-content">
+          <Outlet />
+        </main>
+      </div>
+    </div>
     </>
   )
 }
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -6,6 +6,7 @@ import ProfileLayout from "./Layouts/ProfileLayout"
 import Login from "./Pages/Login/Login"
 import Index from "./Pages/Main/Index/Index"
 import PostDetailPages from "./Pages/Main/PostDetailPages/PostDetailPages"
+import Panel from "./Pages/Panel/Panel"
 import PostUpdatePage from "./Pages/PostUpdate/PostUpdatePage"
 import PostUpdate from "./Pages/PostUpdate/PostUpdatePage"
 import Register from "./Pages/Register/Register"
@@ -21,7 +22,7 @@ const routes = [{
       ],
 },
 {path :'/panel',element:<ProfileLayout/>,children:[
-          {path:'profile',element:<Profile/>},
+          {index:true,element:<Panel/>},
           {path:'post/:id',element:<PostUpdatePage/>}
 
 ]}]

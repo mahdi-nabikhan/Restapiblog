b304a8539418a21d71523ce86ee3017422fe56b0
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-06-12 10:07:07 +0330

    feat(service): add postgressql service  to docker compose refactor(backend): add new setting for data base in settings.py

--- backend/RestApiBlog/settings.py
+++ backend/RestApiBlog/settings.py
@@ -84,12 +84,15 @@ WSGI_APPLICATION = 'RestApiBlog.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blog_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 

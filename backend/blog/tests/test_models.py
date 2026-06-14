400a9f07256bcfd9fec076b06f86d69e4f573d2c
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-06-14 17:48:25 +0330

    feat(Backend):add model test for Comments

--- backend/blog/tests/test_models.py
+++ backend/blog/tests/test_models.py
@@ -1,8 +1,7 @@
 import pytest
 from django.utils import timezone
-from blog.models import Post, Category
+from blog.models import Post, Category,Comments
 from accounts.models import User
-
 @pytest.mark.django_db
 def test_category_str():
     category = Category.objects.create(name="Tech")
@@ -51,3 +50,18 @@ def test_post_get_absolute_api_url(client):
     url = post.get_absolute_api_url()
     response = client.get(url)
     assert response.status_code in [200, 301, 302, 403, 401, 404]  # depends on your view setup
+
+@pytest.mark.django_db
+def test_comment_model():
+    user = User.objects.create_user(email="test@example.com", password="password123")
+    category = Category.objects.create(name="Tech")
+    post = Post.objects.create(
+        auther=user,
+        title="Test Post",
+        content="This is the content of the post.",
+        status=True,
+        category=category,
+        published_date=timezone.now()
+    )
+    comment = Comments.objects.create(post=post,user=user,content ="this is for test")
+    assert comment.user == user

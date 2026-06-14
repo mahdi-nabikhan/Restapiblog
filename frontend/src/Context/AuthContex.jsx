e1ac6a23e21b2c08790dde2767ba4a5ea73c572b
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-06-14 12:14:32 +0330

    feat(frontend):add context for checking a login status

---
+++ frontend/src/Context/AuthContex.jsx
@@ -0,0 +1,48 @@
+import React, { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export default function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const checkAuth = async () => {
+    try {
+      const res = await fetch(
+        "http://localhost:8000/accounts/api/v1/me/",
+        {
+          credentials: "include",
+        }
+      );
+
+      if (!res.ok) {
+        setUser(null);
+        return;
+      }
+
+      const data = await res.json();
+      setUser(data);
+    } catch (err) {
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+        loading,
+        isLoggedIn: !!user,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}

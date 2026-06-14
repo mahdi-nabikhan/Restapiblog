9d7006698044d9ee186a82c306578d8d8dcb0ff4
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-06-14 17:27:21 +0330

    refactor(Backend):add Custom Admin for Profile model

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -24,6 +24,44 @@ class CustomUserAdmin(BaseUserAdmin):
         }),
     )
 
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "first_name",
+        "last_name",
+        "updated_date",
+    )
 
+    list_filter = (
+        "created_date",
+        "updated_date",
+    )
+
+    search_fields = (
+        "user__email",
+        "first_name",
+        "last_name",
+    )
+
+    readonly_fields = (
+        "created_date",
+        "updated_date",
+    )
+
+    fieldsets = (
+        ("User Info", {
+            "fields": ("user",)
+        }),
+        ("Personal Info", {
+            "fields": ("first_name", "last_name", "image", "description")
+        }),
+        ("Timestamps", {
+            "fields": ("created_date", "updated_date")
+        }),
+    )
+
+    ordering = ("-updated_date",)
 admin.site.register(User, CustomUserAdmin)
-admin.site.register(Profile)
+

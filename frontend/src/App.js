d576d1ceb8ae70fa01cffdf62c43d5cfbbd356df
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-06-22 12:58:21 +0330

    reactor(frontend): add React Query to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
-
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import './App.css';
 import { useRoutes } from 'react-router-dom';
 import routes from './routes';
+const queryClient = new QueryClient()
 function App() {
+  
   const router = useRoutes(routes)
   return (
-    <>{router}</>
+    <><QueryClientProvider client={queryClient}>
+      {router}
+    </QueryClientProvider>
+    
+    </>
   );
 }
 

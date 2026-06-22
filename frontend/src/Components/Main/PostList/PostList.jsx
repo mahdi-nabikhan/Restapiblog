00f9921c9603e98fc84f0277610dbb7a31403a99
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-06-22 13:12:14 +0330

    reafactor(frontend): add react query to PostList.jsx Component

--- frontend/src/Components/Main/PostList/PostList.jsx
+++ frontend/src/Components/Main/PostList/PostList.jsx
@@ -1,71 +1,70 @@
 import React, { useEffect, useState } from "react";
+import BACKEND_URL from "../../../Utils";
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import "./PostList.css";
 import Pagination from "../../../Components/Pagination/Pagination";
 import BACKEND_URL from "../../../Utils";
 export default function PostList() {
-  const [posts,setPosts]=useState([])
+
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-
   const pageSize = 1;
 
-  const fetchPosts = async () => {
-    try {
-      const res = await fetch(
-        `${BACKEND_URL}/blog/api/v1/post/?page=${page}&page_size=${pageSize}`,
-        {
-          credentials:'include',
-          method: "GET",
-        }
-      );
-
-      const data = await res.json();
-      setPosts(data);
-    } catch (err) {
-      console.log(err);
-    }
+  const getPosts = async (page) => {
+    const res = await fetch(
+      `${BACKEND_URL}/blog/api/v1/post/?page=${page}&page_size=${pageSize}`,
+      {
+        credentials: "include",
+      }
+    );
+  
+    return res.json();
   };
-
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["posts", page],
+    queryFn: () => getPosts(page),
+  });
   useEffect(() => {
-    fetchPosts();
-  }, []);
-
+    if (data?.count) {
+      setTotalPages(Math.ceil(data.count / pageSize));
+    }
+  }, [data]);
 
   return (
     <div className="posts-container">
-  <h2 className="posts-title">Latest Posts</h2>
+      <h2 className="posts-title">Latest Posts</h2>
 
-  <div className="posts-grid">
-    {posts.map((post) => (
-      <div className="post-card" key={post.id}>
+      <div className="posts-grid">
+        {data?.results?.map((post) => (
+          <div className="post-card" key={post.id}>
 
-        <img
-          className="post-image"
-          src={post.image || "/images/default_image.PNG"}
-          alt={post.title}
-          onError={(e) => {
-            e.target.src = "/images/default_image.PNG";
-          }}
-        />
+            <img
+              className="post-image"
+              src={post.image || "/images/default_image.PNG"}
+              alt={post.title}
+              onError={(e) => {
+                e.target.src = "/images/default_image.PNG";
+              }}
+            />
 
-        <h3>Title : {post.title}</h3>
+            <h3>Title : {post.title}</h3>
 
-        <p>
-          Description : {post.snippet}
-        </p>
-        <p>Published at : {post.created_date}</p>
-        
-        <Link to={`post/${post.id}`}>Detail</Link>
-        
+            <p>
+              Description : {post.snippet}
+            </p>
+            <p>Published at : {post.created_date}</p>
+
+            <Link to={`post/${post.id}`}>Detail</Link>
+
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-  <Pagination
+      <Pagination
         currentPage={page}
         totalPages={totalPages}
         onPageChange={setPage}
       />
-</div>
+    </div>
   );
 }

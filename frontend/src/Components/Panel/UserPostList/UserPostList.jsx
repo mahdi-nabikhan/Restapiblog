1cf727c0dd115fbde38a383f8d228bb3098caab4
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-06-24 11:35:19 +0330

    feat(frontend):Add React query for geting post data related with user  for panel

--- frontend/src/Components/Panel/UserPostList/UserPostList.jsx
+++ frontend/src/Components/Panel/UserPostList/UserPostList.jsx
@@ -1,41 +1,56 @@
 import React from 'react'
 import './UserPostList.css'
+import { useQuery } from '@tanstack/react-query'
+import BACKEND_URL from "../../../Utils";
 export default function UserPostList() {
-  return (
-    <>
-        <div class="user-posts-wrapper">
+   const UserPosts= async function(){
+      const res = await fetch (`${BACKEND_URL}//blog/api/v1/user/post/`,{
+        credentials:'include'
+      })
+      const data = await res.json();
+      console.log(data);
+      return data;
+   }
 
-<div class="user-posts">
 
-  <h2 class="user-posts__title">My Posts</h2>
+   const { data, isLoading, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: UserPosts,
+  });
 
-  <div class="user-posts__list">
 
-    <div class="post-item">
-      <img src="/images/default_image.PNG" />
+  
+  if (isLoading) return <h2>Loading...</h2>;
 
-      <div class="post-info">
-        <h3>Post Title</h3>
-        <p>Short snippet of post content...</p>
+if (error) return <h2>Error...</h2>;
 
-        <div class="post-meta">
-          <span>Tech</span>
-          <span>Published</span>
-        </div>
-      </div>
+return (
+  <table className="posts-table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Date</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
 
-      <div class="post-actions">
-        <button>Edit</button>
-        <button>Delete</button>
-      </div>
-    </div>
+    <tbody>
+      {data?.map((post) => (
+        <tr key={post.id}>
+          <td>{post.id}</td>
+          <td>{post.title}</td>
+          <td>{post.author}</td>
+          <td>{post.created_date}</td>
 
-  </div>
-
-</div>
-
-</div>
-    
-    </>
-  )
+          <td>
+            <button>Edit</button>
+            <button>Delete</button>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
 }

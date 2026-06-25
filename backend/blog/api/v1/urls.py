80bb35bec23ec13c471816f551bb42bfa5591048
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-06-25 10:08:57 +0330

    fix some bugs on backend side

--- backend/blog/api/v1/urls.py
+++ backend/blog/api/v1/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('comment/detail/<int:pk>/',CommentDetailAndDeleteAPIView.as_view(),name='comment-detail'),
     path('user/post/',UserPostListApiView.as_view(),name='user-posts'),
     path('post/list/cache/', PostListCacheAPIView.as_view(),name='post-cache'),
-    path('img/post/<ibt:pk>/',PostImageCreateAndListAPIView.as_view(),name='img_post'),
+    path('img/post/<int:pk>/',PostImageCreateAndListAPIView.as_view(),name='img_post'),
     path('', include(router.urls)),
 
 ]
